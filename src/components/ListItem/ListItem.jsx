d6444a5047802faa928001a709b779fd809c809d
Linssi Colin Ruiz <linssi101@gmail.com>
2023-01-04 20:08:01 -0600

    created component for ListItem

---
+++ src/components/ListItem/ListItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ListItem (props) {
+
+    return (
+        // <>
+        // {JSON.stringify(props.creature)}
+        // </>
+     <li>
+        <button>delete me</button>
+        {props.creature.name} is from {props.creature.origin}
+        <button>click me</button>
+     </li>
+    )
+}
+
+export default ListItem;
